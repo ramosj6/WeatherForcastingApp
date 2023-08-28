309bb7cf19d0753721b78ec82423f8ed591ab9e8
Michael Pina <mpina1994@aol.com>
2023-08-28 11:07:12 -0500

    Added my Location_fetch

--- mongo_connect.py
+++ mongo_connect.py
@@ -1,9 +1,9 @@
+import os
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-uri = "mongodb+srv://team5:team5pass@cluster0.i0xzt0l.mongodb.net/?retryWrites=true&w=majority"
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-# Send a ping to confirm a successful connection
+#Create a new client and connect to the server
+client = MongoClient(os.environ.get("uri"), server_api=ServerApi('1'))
+#Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
