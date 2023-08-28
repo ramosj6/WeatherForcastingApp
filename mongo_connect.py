6b420beae00f1884b7622882e99ece8d783bf611
Jesus Ramos <jesusramos.jr1999@gmail.com>
2023-08-28 11:06:55 -0500

    Deleted mongo_connect file from .gitignore

---
+++ mongo_connect.py
@@ -0,0 +1,13 @@
+import os
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+#Create a new client and connect to the server
+client = MongoClient(os.environ.get("uri"), server_api=ServerApi('1'))
+
+#Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
