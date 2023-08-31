d929886c63c10a0a74ba0f485fdc35f98cfca92e
Michael Pina <mpina1994@aol.com>
2023-08-31 14:44:02 -0500

    added certifi

--- mongo_connect.py
+++ mongo_connect.py
@@ -1,10 +1,12 @@
 import os
-import certifi
+import certifi 
 
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+
 #Create a new client and connect to the server
 client = MongoClient(os.environ.get("uri"), server_api=ServerApi('1'), tlsCAFile=certifi.where())
+
 #Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
